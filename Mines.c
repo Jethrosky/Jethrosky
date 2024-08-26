11e0fc2861d2c51c2383eff391c48855156b0c8f
Arthur Mavheneka <arthurmavheneka@Arthurs-MacBook-Pro.local>
2024-08-26 13:34:00 +0200

    arthur

---
+++ Mines.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char operator[2]; // Assuming operators are single characters
+    double num1;
+    double num2;
+    double result;
+
+    printf("\nEnter an operator(+ - * /): ");
+    scanf("%s", operator);
+
+    printf("Enter Number 1: ");
+    scanf("%lf", &num1);
+
+    printf("\nEnter Number 2: ");
+    scanf("%lf", &num2);
+
+    switch (operator[0]) { // Access the first character of the operator
+        case '+':
+            result = num1 + num2;
+            printf("\nresult: %lf", result);
+            break;
+
+        // ... other cases remain the same
+
+        default:
+            printf("%s is not valid", operator);
+    }
+
+    return 0;
+}
