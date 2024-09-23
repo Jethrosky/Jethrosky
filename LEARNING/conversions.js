ca2c9295568246b0c591a35d9392038ba2d08970
Jethrosky <loodabeats@gmail.com>
2024-09-23 10:46:52 +0200

    if else

---
+++ LEARNING/conversions.js
@@ -0,0 +1,7 @@
+const myAge = `16`
+
+if (Number(myAge)>=18) {
+    console.log(`Yes you are eligible to get a license at the age of ${myAge}`)
+} else {
+    console.log(`No you are not eligible to get a license at the age of ${myAge}`)
+}
